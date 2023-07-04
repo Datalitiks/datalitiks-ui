20313b0235acb36d0e2b5f3f7f939f89b53d83cf
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2023-07-04 11:34:47 +0200

    All confugured and compiling successfully

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,45 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+const path = require('path');
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { ProgressPlugin } = require('webpack');
 
-const isProduction = process.env.NODE_ENV == "production";
-
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
-
-const config = {
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-  },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = "development";
-  }
-  return config;
-};
+module.exports = function(env, argv) {
+    return {
+        mode: argv.mode,
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        output: {
+            filename: 'index.js',
+            library: '$',
+            libraryTarget: 'umd',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.(js|ts)x?$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'ts-loader']
+                }
+            ]
+        },
+        externals: {
+            'react': 'react',
+            'react-dom': 'react-dom'
+        },
+        resolve: {
+            alias: {
+                '@root': path.resolve(__dirname, 'src')
+            },
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        plugins: [
+            new ProgressPlugin(),
+            new ESLintPlugin({
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }),
+            new CleanWebpackPlugin()
+        ]
+    };
+}

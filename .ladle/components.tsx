e188395ac038641b129054779f07e6a4fdc7874c
Ian Jamieson <ian@jimjam.dev>
2023-07-12 13:14:25 +0200

    styles fixed

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -5,7 +5,8 @@ import type { GlobalProvider } from "@ladle/react";
 export const Provider: GlobalProvider = ({
   children,
 }) => (
-  <>
+  <div>
+    <h1>Test Global</h1>
     {children}
-  </>
+  </div>
 );

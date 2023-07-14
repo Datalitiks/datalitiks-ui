543cda4bdf8a0eca4fe8b2c1b0108cb2fba5e9ef
Ian Jamieson <ian@jimjam.dev>
2023-07-14 16:35:31 +0200

    ts config paths

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'tsup'
+import { tsconfigPathsPlugin } from "esbuild-plugin-tsconfig-paths";
 
 export default defineConfig({
-    outExtension({ format }) {
-        return {
-          js: `.${format}.js`,
-        }
-    },
+  outExtension({ format }) {
+    return {
+      js: `.${format}.js`,
+    }
+  },
   entry: ['src/index.ts'],
   outDir: 'dist',
-  splitting: false,
+  splitting: true,
+  external: ['react', 'react-dom'],
+  tsconfig: 'tsconfig.json',
   sourcemap: true,
   clean: true,
+  bundle: true,
+  esbuildPlugins: [tsconfigPathsPlugin()],
 })

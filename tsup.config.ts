96239de90060db102874b803e7c3c52e12242758
Ian Jamieson <ian@jimjam.dev>
2023-07-17 23:50:32 +0200

    update imports for library mode

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   outDir: 'dist',
   format: ['cjs', 'esm'],
   bundle: false,
+  external: ['@datalitiks/datalitiks-ui'],
   entry: ['./src/index.ts', "./src/**/*.{ts,tsx}"],
   esbuildOptions(options) {
     // the directory structure will be the same as the source

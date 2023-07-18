a7a9c1240c8593ce1887942ffec16f20f16cac86
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2023-07-18 11:50:53 +0200

    added import react

--- src/components/forms/textarea/index.tsx
+++ src/components/forms/textarea/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+import React from 'react';
 import { cx } from '@/styled-system/css';
 import { styled } from '@/styled-system/jsx';
 import { TextareaProps, textareaRecipe } from './textarea.recipe';
